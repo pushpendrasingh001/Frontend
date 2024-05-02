ff4cf9632141fdf9b692cc91b42510e2d1d5f469
pushpendra singh <139163856+pushpendrasingh001@users.noreply.github.com>
2024-05-02 14:21:28 +0530

    Update Cards.jsx card url

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -7,7 +7,7 @@ function Cards({item}) {
       <div className='mt-4 my-4 p-3 flex justify-content: space-evenly;'>
       <div className="card w-92  bg-base-100 shadow-xl hover:scale-105 duration-200 dark:bg-slate-900 dark:text-white dark:border">
   <figure >
-    <img src={item.image} alt=" card missing" />
+    <img src={{item.image} || "https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg?t=st=1714639053~exp=1714642653~hmac=27441614cdd2925cb879470cf015b0b72673efeca2d3533d4c1926956e5c3b95&w=740" }alt=" card missing" />
   </figure>
   <div className="card-body">
     <h2 className="card-title">
