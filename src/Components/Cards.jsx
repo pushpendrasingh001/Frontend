0c31a4b93bb093dc9bf710a79d9a6e0a08b14a3e
pushpendra singh <139163856+pushpendrasingh001@users.noreply.github.com>
2024-05-02 13:09:02 +0530

    Update Cards.jsx card image url change

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -7,7 +7,7 @@ function Cards({item}) {
       <div className='mt-4 my-4 p-3 flex justify-content: space-evenly;'>
       <div className="card w-92  bg-base-100 shadow-xl hover:scale-105 duration-200 dark:bg-slate-900 dark:text-white dark:border">
   <figure >
-    <img src="../../public/card-img.jpg" alt=" card missing" />
+    <img src={item.image} alt=" card missing" />
   </figure>
   <div className="card-body">
     <h2 className="card-title">
